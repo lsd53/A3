3ad6d3e60cfda83ff09b88fce1e7f29bd4758206
Luis Gustavo Santos De Medeiros <lsd53@en-cs-uglab01.coecis.cornell.edu>
2015-02-18 20:46:15 -0500

    Done with Frankenstein

--- frankenprepos.sh
+++ frankenprepos.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 cat frankenstein.txt|  tr -s '[[:punct:][:space:]]' '\n'   | tr '[A-Z]' '[a-z]' | sort -f |uniq -c  | sort -n> sorted.txt 
-grep  sorted.txt
+grep --color -w -v  -f prepositions.txt sorted.txt|tail -100
 
