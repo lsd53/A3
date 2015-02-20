dd8264bb047dac3dce5b2cc2d5ce7d45f42473c6
kevinschaich <schaich.kevin@gmail.com>
2015-02-19 19:52:38 -0500

    teamwords.sh done

--- teamwords.sh
+++ teamwords.sh
@@ -1,2 +1,14 @@
 #! /bin/bash
-cat superbowl.html | grep -i --color '\(seahawks\)\|\(patriots\)'
+
+usage ()
+{
+  echo 'Usage : <nbefore> <nafter'
+  exit
+}
+
+if [ "$#" -ne 2 ]
+then
+  usage
+fi
+
+cat superbowl.html | tr -s '[[:punct:][:space:]]' '\n' | grep -i -A $2 -B $1 --color '\(seahawks\)\|\(patriots\)' | tr -s '\n' ' ' | tr -s '-' '\n' | sed  '/^ $/d'
