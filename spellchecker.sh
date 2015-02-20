6c981ef2fb8b2511216f5c7070aca811a5209a2d
kevinschaich <schaich.kevin@gmail.com>
2015-02-20 16:38:55 -0500

    spellchecker.sh done

--- spellchecker.sh
+++ spellchecker.sh
@@ -1,8 +1,18 @@
 #! /bin/bash 
-cat $1 |tr -s '[[:punct:][:space:]]' '\n' > temp.txt
-cat english.txt| tr -s '\n' '|' >english1.txt
 
-grep -Ewvf english1.txt temp.txt
+usage ()
+{
+  echo 'Usage : <input file>'
+  exit
+}
 
+if [ "$#" -ne 1 ]
+then
+  usage
+fi
 
-
+cat $1 | tr -s '[[:punct:][:space:]]' '\n' > temp.txt
+grep -xf temp.txt english.txt > correct.txt
+grep -vxf correct.txt temp.txt
+rm temp.txt
+rm correct.txt
